ff980295f08b4ace6aa28b86116a20b7655818c9
Peter Wilson <peterrwilson99@gmail.com>
2023-09-07 16:47:00 -0700

    merged main and moved toolbox to the right

--- src/components/MUI-Form-Builder/Builder.tsx
+++ src/components/MUI-Form-Builder/Builder.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from 'react'
 import Toolbox from './Toolbox';
 import { ComponentDefaults, Components } from './elements/Components';
-import { Box, Button, Drawer, IconButton, Paper, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { Box, Button, Drawer, Grid, IconButton, Paper, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
@@ -103,72 +103,78 @@ const Builder: FC<BuilderProps> = (props) => {
 
     return (
         <div className="builder">
-            <Box className="preview" justifyContent="flex-end">
-                <ToggleButtonGroup
-                    color="primary"
-                    value={mode}
-                    exclusive
-                    onChange={handleModeChange}
-                    >
-                    <ToggleButton value="builder">Builder</ToggleButton>
-                    <ToggleButton value="preview" disabled={elements.length === 0}>Preview</ToggleButton>
-                </ToggleButtonGroup>
-            </Box>
-            {mode === "builder" ?
-                <div>
-                    <div className="form-preview">
-                        {elements.map((element, index) => {
-                            const Component = Components[element.type as keyof typeof Components];
-                            
-                            return (
-                                <Paper elevation={2} sx={{ p: 1, my: 1 }} >
-                                    <div>
-                                        <Box sx={{display: "flex", justifyContent: "end", marginBottom: "-35px"}}>
-                                            <IconButton onClick={() => openDrawer(element.id)}> 
-                                                <SettingsIcon />
-                                            </IconButton>
-                                            <IconButton onClick={() => moveElementUp(index)} disabled={index === 0}>
-                                                <ArrowUpwardIcon />
-                                            </IconButton>
-                                            <IconButton onClick={() => moveElementDown(index)} disabled={index === elements.length - 1}>
-                                                <ArrowDownwardIcon />
-                                            </IconButton>
-                                            <IconButton onClick={() => deleteElement(element.id)}>
-                                                <DeleteIcon />
-                                            </IconButton>
-                                        </Box>
-                                        <Component {...element as any} disabled={true} />
-                                    </div>
-                                </Paper>
-                            );
-                        })}
-                    </div>
-                    <div className="toolbox">
-                        <Toolbox onAddComponent={onAddComponent}/>
-                    </div>
-                    <Drawer anchor="right" open={drawerOpen} onClose={closeDrawer}>
-                        <IconButton onClick={closeDrawer} style={{ position: 'absolute', left: 0, margin: '10px' }}>
-                            <CloseIcon />
-                        </IconButton>
-                        {
-                            elements[activeElement] ?
-                                <Properties element={elements[activeElement]} editElement={editElement} />
-                                :
-                                <></>
+            <Grid container spacing={4}>
+                <Grid item xs={12} md={9}>
+                    <div className="workspace">
+                        {mode === "builder" ?
+                            <React.Fragment>
+                                <div className="form-preview">
+                                    {elements.map((element, index) => {
+                                        const Component = Components[element.type as keyof typeof Components];
+                    
+                                        return (
+                                            <Paper elevation={2} sx={{ p: 1, my: 1 }} >
+                                                <div>
+                                                    <Box sx={{display: "flex", justifyContent: "end", marginBottom: "-35px"}}>
+                                                        <IconButton onClick={() => openDrawer(element.id)}>
+                                                            <SettingsIcon />
+                                                        </IconButton>
+                                                        <IconButton onClick={() => moveElementUp(index)} disabled={index === 0}>
+                                                            <ArrowUpwardIcon />
+                                                        </IconButton>
+                                                        <IconButton onClick={() => moveElementDown(index)} disabled={index === elements.length - 1}>
+                                                            <ArrowDownwardIcon />
+                                                        </IconButton>
+                                                        <IconButton onClick={() => deleteElement(element.id)}>
+                                                            <DeleteIcon />
+                                                        </IconButton>
+                                                    </Box>
+                                                    <Component {...element as any} disabled={true} />
+                                                </div>
+                                            </Paper>
+                                        );
+                                    })}
+                                </div>
+                    
+                                <Drawer anchor="right" open={drawerOpen} onClose={closeDrawer}>
+                                    <IconButton onClick={closeDrawer} style={{ position: 'absolute', left: 0, margin: '10px' }}>
+                                        <CloseIcon />
+                                    </IconButton>
+                                    {
+                                        elements[activeElement] ?
+                                            <Properties element={elements[activeElement]} editElement={editElement} />
+                                            :
+                                            <></>
+                                    }
+                                </Drawer>
+                                <Button sx={{marginY: "16px"}} onClick={() => { saveForm ? saveForm(elements) : console.log(elements) }} variant="outlined">
+                                    Save Form
+                                </Button>
+                            </React.Fragment>
+                            :
+                            <Viewer form={elements} preview={true}/>
                         }
-                    </Drawer>
-                    <Button sx={{marginY: "16px"}} onClick={() => { saveForm ? saveForm(elements) : console.log(elements) }} variant="outlined">
-                        Save Form
-                    </Button>
-                </div>
-                :
-                <Viewer form={elements} preview={true}/>
-            }
-            {/* {mode === "preview" ? 
-                <Viewer form={elements} preview={true}/>
-                :
-                <></>
-            } */}
+                    </div>
+                </Grid>
+                <Grid item xs={12} md={2}>
+                    <Box className="controls">
+                        <ToggleButtonGroup
+                            fullWidth
+                            color="primary"
+                            value={mode}
+                            exclusive
+                            onChange={handleModeChange}
+                        >
+                            <ToggleButton value="builder">Builder</ToggleButton>
+                            <ToggleButton value="preview" disabled={elements.length === 0}>Preview</ToggleButton>
+                        </ToggleButtonGroup>
+                        <Box className="toolbox" sx={{display: "flex", justifyContent: 'flex-end', paddingTop: 2}}>
+                            <Toolbox onAddComponent={onAddComponent}/>
+                        </Box>
+                    </Box>
+                </Grid>
+                
+            </Grid>
         </div>
     );
 }
