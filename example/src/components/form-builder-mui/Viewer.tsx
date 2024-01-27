30631d596982842e4aea1d4f0496e107fecf4742
Peter Wilson <peterrwilson99@gmail.com>
2024-01-26 18:05:35 -0800

    wip almost completed

--- example/src/components/form-builder-mui/Viewer.tsx
+++ example/src/components/form-builder-mui/Viewer.tsx
@@ -1,5 +1,6 @@
-import React, { FC, FormEvent, useState } from "react";
-import { Button } from "@mui/material";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { FC, FormEvent, useCallback, useEffect, useState } from "react";
+import { Button, debounce } from "@mui/material";
 import { Components } from "./elements/Components";
 
 export interface Element {
@@ -14,42 +15,63 @@ export interface ViewerProps {
   onSubmitPartial?: (formValues: any) => void;
   preview?: Boolean;
   disabled?: boolean;
+  submitText?: string;
+  saveText?: string;
+}
+interface FormValues {
+  [key: string]: any;
 }
 
-const Viewer: FC<ViewerProps> = ({ form, onSubmit, onSubmitPartial, preview, disabled }) => {
-  const [formValues, setFormValues] = useState([]);
-  const elements = form ?? [];
+const Viewer: FC<ViewerProps> = ({ form, onSubmit, onSubmitPartial, preview, disabled, submitText, saveText }) => {
+  const [formValues, setFormValues] = useState<FormValues>({});
+  const [triggerAutosave, setTriggerAutosave] = useState(false);
+  const elements = form ?? [] as Element[];
+
   const handleChange = (id: string, value: any) => {
     setFormValues((prevValues) => ({
       ...prevValues,
       [id]: value,
     }));
+    setTriggerAutosave(true);
   };
 
-  const getCompletedForm = () => {
+  React.useEffect(() => {
+    if(triggerAutosave && !preview && !disabled && onSubmitPartial){
+      autoSave(formValues);
+      setTriggerAutosave(false);
+    }
+  }, [triggerAutosave]);
+
+  const getCompletedForm = (formValuesProp: FormValues | undefined) => {
     const completedForm = form ?? [];
-    for(const id in formValues){
+    const curFormValues = formValuesProp ?? formValues;
+    for(const id in curFormValues){
       const index = completedForm.findIndex((element) => String(element.id) === id );
-      completedForm[index].value = formValues[id];
+      completedForm[index].value = curFormValues[id];
     }
     return completedForm;
   }
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    const completedForm = getCompletedForm();
+  const handleSubmit = (e: FormEvent | undefined) => {
+    if(e) e.preventDefault();
+    const completedForm = getCompletedForm(undefined);
     if (onSubmit) {
       onSubmit(completedForm);
     }
   };
-  const handleSave = (e: FormEvent) => {
-    e.preventDefault();
-    const completedForm = getCompletedForm();
+  const handleSave = (e: FormEvent | undefined, formValuesProp?: FormValues | undefined) => {
+    if(e) e.preventDefault();
+    const completedForm = getCompletedForm(formValuesProp);
     if (onSubmitPartial) {
       onSubmitPartial(completedForm);
     }
   };
 
+  const autoSave = React.useMemo(() => {
+    return debounce((formValuesProp) => handleSave(undefined, formValuesProp), 2000);
+  }, []);
+
+  
   return (
     <form onSubmit={handleSubmit}>
       {elements.map((element, index) => {
@@ -59,11 +81,11 @@ const Viewer: FC<ViewerProps> = ({ form, onSubmit, onSubmitPartial, preview, dis
       {!preview && !disabled ? 
         <React.Fragment>
           <Button type="submit" variant="outlined" sx={{marginBottom: '1rem'}} >
-            Submit
+            {submitText ?? 'Submit'}
           </Button>
           {onSubmitPartial ?
             <Button variant="outlined" onClick={handleSave} sx={{marginLeft: '1rem', marginBottom: '1rem'}} color="secondary" >
-              Save
+              {saveText ?? 'Save'}
             </Button>
             :
             <></>
